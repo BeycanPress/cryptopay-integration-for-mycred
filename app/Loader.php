2eb4336101a42ae9ab7e13d2023c1c0417dec1d9
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-03-26 16:39:33 +0800

    first commit

---
+++ app/Loader.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BeycanPress\CryptoPay\MyCred;
+
+class Loader
+{
+    /**
+     * Loader constructor.
+     */
+    public function __construct()
+    {
+    }
+}
---
+++ mycred-cryptopay-integration.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+defined('ABSPATH') || exit;
+
+// @phpcs:disable PSR1.Files.SideEffects
+// @phpcs:disable PSR12.Files.FileHeader
+// @phpcs:disable Generic.Files.InlineHTML
+// @phpcs:disable Generic.Files.LineLength
+
+/**
+ * Plugin Name: myCred - CryptoPay Gateway
+ * Version:     1.0.0
+ * Plugin URI:  https://beycanpress.com/cryptopay/
+ * Description: Adds Cryptocurrency payment gateway (CryptoPay) and crypto withdrawal for myCred.
+ * Author:      BeycanPress LLC
+ * Author URI:  https://beycanpress.com
+ * License:     GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: mycred-cryptopay
+ * Tags: Cryptopay, Cryptocurrency, WooCommerce, WordPress, MetaMask, Trust, Binance, Wallet, Ethereum, Bitcoin, Binance smart chain, Payment, Plugin, Gateway, Moralis, Converter, API, coin market cap, CMC
+ * Requires at least: 5.0
+ * Tested up to: 6.4.3
+ * Requires PHP: 8.1
+*/
+
+// Autoload
+require_once __DIR__ . '/vendor/autoload.php';
+
+define('MYCRED_CRYPTOPAY_FILE', __FILE__);
+define('MYCRED_CRYPTOPAY_VERSION', '1.0.0');
+define('MYCRED_CRYPTOPAY_KEY', basename(__DIR__));
+define('MYCRED_CRYPTOPAY_URL', plugin_dir_url(__FILE__));
+define('MYCRED_CRYPTOPAY_DIR', plugin_dir_path(__FILE__));
+define('MYCRED_CRYPTOPAY_SLUG', plugin_basename(__FILE__));
+
+use BeycanPress\CryptoPay\Integrator\Helpers;
+
+Helpers::registerModel(BeycanPress\CryptoPay\MyCred\Models\TransactionsPro::class);
+Helpers::registerLiteModel(BeycanPress\CryptoPay\MyCred\Models\TransactionsLite::class);
+
+load_plugin_textdomain('mycred-cryptopay', false, basename(__DIR__) . '/languages');
+
+if (!defined('GF_MIN_WP_VERSION') /* TODO: check the plugin */) {
+    Helpers::requirePluginMessage('myCred', 'https://wordpress.org/plugins/mycred/');
+} elseif (Helpers::bothExists()) {
+    new BeycanPress\CryptoPay\MyCred\Loader();
+} else {
+    Helpers::requireCryptoPayMessage('myCred');
+}
