48bff2b647daf3d737a3536af45970a5d2be74a5
BeycanDeveloper <halilbeycan0@gmail.com>
2024-11-28 13:27:55 +0300

    fixed text domain warning

--- cryptopay-integration-for-mycred.php
+++ cryptopay-integration-for-mycred.php
@@ -11,7 +11,7 @@ defined('ABSPATH') || exit;
 
 /**
  * Plugin Name: CryptoPay Integration for myCred
- * Version:     1.0.0
+ * Version:     1.0.1
  * Plugin URI:  https://beycanpress.com/cryptopay/
  * Description: Adds Cryptocurrency payment gateway (CryptoPay) and crypto withdrawal for myCred.
  * Author:      BeycanPress LLC
@@ -21,7 +21,7 @@ defined('ABSPATH') || exit;
  * Text Domain: cryptopay-integration-for-mycred
  * Tags: Bitcoin, Ethereum, Crypto, Payment, myCred
  * Requires at least: 5.0
- * Tested up to: 6.6
+ * Tested up to: 6.7.1
  * Requires PHP: 8.1
 */
 
@@ -29,7 +29,7 @@ defined('ABSPATH') || exit;
 require_once __DIR__ . '/vendor/autoload.php';
 
 define('MYCRED_CRYPTOPAY_FILE', __FILE__);
-define('MYCRED_CRYPTOPAY_VERSION', '1.0.0');
+define('MYCRED_CRYPTOPAY_VERSION', '1.0.1');
 define('MYCRED_CRYPTOPAY_KEY', basename(__DIR__));
 define('MYCRED_CRYPTOPAY_URL', plugin_dir_url(__FILE__));
 define('MYCRED_CRYPTOPAY_DIR', plugin_dir_path(__FILE__));
@@ -48,13 +48,15 @@ function myCredCryptoPayRegisterAddons(): void
 
 myCredCryptoPayRegisterAddons();
 
-load_plugin_textdomain('cryptopay-integration-for-mycred', false, basename(__DIR__) . '/languages');
+add_action('init', function (): void {
+    load_plugin_textdomain('cryptopay-integration-for-mycred', false, basename(__DIR__) . '/languages');
+});
 
 add_action('plugins_loaded', function (): void {
     myCredCryptoPayRegisterAddons();
 
     if (!class_exists('myCRED_Core')) {
-        Helpers::requirePluginMessage('myCred', 'https://wordpress.org/plugins/mycred/');
+        Helpers::requirePluginMessage('myCred', admin_url('plugin-install.php?s=myCred&tab=search&type=term'));
     } elseif (Helpers::bothExists()) {
         new BeycanPress\CryptoPay\MyCred\Loader();
     } else {
